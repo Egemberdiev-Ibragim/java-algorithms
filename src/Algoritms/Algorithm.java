794e28c495ed075afc000c6a655c840422c4a1c1
Egemberdiev-Ibragim <inthistoru@gmail.com>
2023-10-08 09:22:16 +0600

    "Refactoring code"

--- src/Algoritms/Algorithm.java
+++ src/Algoritms/Algorithm.java
@@ -1,10 +1,10 @@
 package Algoritms;
 
 public class Algorithm {
-    public static void bubbleSort(int[] arr){
-        for (int first = 0; first < arr.length; first++){
-            for (int second = first + 1; second < arr.length; second++){
-                if (arr[first] > arr[second]){
+    public static void bubbleSort(int[] arr) {
+        for (int first = 0; first < arr.length; first++) {
+            for (int second = first + 1; second < arr.length; second++) {
+                if (arr[first] > arr[second]) {
                     int temp = arr[first];
                     arr[first] = arr[second];
                     arr[second] = temp;
@@ -12,16 +12,17 @@ public class Algorithm {
             }
         }
     }
-    public static int binarySearch(int[] arr, int elementToSearch){
+
+    public static int binarySearch(int[] arr, int elementToSearch) {
         int first = 0;
-        int last = arr.length -1;
-        while (first <= last){
+        int last = arr.length - 1;
+        while (first <= last) {
             int middle = last + (first - last) / 2;
-            if (arr[middle] == elementToSearch){
+            if (arr[middle] == elementToSearch) {
                 return middle;
-            }else if (arr[middle] < elementToSearch) {
+            } else if (arr[middle] < elementToSearch) {
                 first = middle + 1;
-            }else if (arr[middle] > elementToSearch) {
+            } else if (arr[middle] > elementToSearch) {
                 first = middle - 1;
             }
         }
